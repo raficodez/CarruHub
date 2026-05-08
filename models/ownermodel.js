20f48adce5b026845594f5c758432dd80c6399a8
raficodez <shaikrafi9325@gmail.com>
2026-05-08 11:12:03 +0530

    update mongodb url and modify routes

--- models/ownermodel.js
+++ models/ownermodel.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
 
-
 const ownerSchema = mongoose.Schema({
     fullname:String,
     email:String,
--- routes/ownersRoute.js
+++ routes/ownersRoute.js
@@ -1,8 +1,28 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router()
+const ownerModel = require("../models/ownermodel")
 
-router.get('/',function(req,res){
+if (process.env.NODE_ENV === "development") {
+    router.post('/create', async function (req, res) {
+        let owner = await ownerModel.find();
+        if (owner.length > 0) {
+            return res.status(503).send("You don't have to permission to create a new owner.")
+        }
+        let {  fullname, email, password} = req.body;
+
+        let createdOwner = await ownerModel.create({
+            fullname,
+            email,
+            password
+        })
+        res.status(201).send(createdOwner)
+    })
+};
+
+router.get('/', function (req, res) {
     res.send("hey its working , owners")
 })
+
+
 
 module.exports = router;
